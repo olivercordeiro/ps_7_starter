05968f016d706d7198df874aeb1b1c70c10f3c98
Oliver Cordeiro <olivercordeiro@Olivers-MacBook-Pro-2.local>
2018-11-14 14:10:08 -0500

    mr. sakmar lost the push up contest :(

--- process_data.R
+++ process_data.R
@@ -1,1 +1,35 @@
-p]
+#mv ~/Downloads/mt_2_results.csv .
+library(tidyverse)
+library(dplyr)
+library(knitr)
+library(fs)
+library(janitor)
+library(scales)
+library(stringr)
+
+js <- read_csv("mt_2_results.csv")
+js <- js %>% 
+  unite("statedistrict", c("state", "district"), sep ="")
+
+
+
+download.file(url = "https://goo.gl/ZRCBda",
+                            destfile = "master.zip",
+                            quiet = TRUE,
+                            mode = "wb")
+
+filenames <- dir_ls("2018-live-poll-results-master/data/")
+
+house <- map_dfr(filenames, read_csv, .id = "source")
+
+house <- house %>% 
+  filter(! str_detect(source, pattern = "gov"),
+         ! str_detect(source, pattern = "sen")) %>% 
+  separate(source, c("a", "b", "c", "d", "e", "f", "g", "h", "statedistrict", "wave", "k")) %>% 
+  select(-a, -b, -c, -d, -e, -f, -g, -h, -k) %>% 
+  filter(wave == "3")
+house$statedistrict <- str_to_upper(house$statedistrict)
+
+
+combined <- left_join(house, js, by = "statedistrict")
+combined
